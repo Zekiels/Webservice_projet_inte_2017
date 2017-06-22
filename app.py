d2b823478381be3e5509bd330d06b63e42cdb41f
Zekiels <mama.du33@live.fr>
2017-06-22 16:24:30 +0200

    Ajout de la route weather pour la meteo entre le programme C et le serveur

--- app.py
+++ app.py
@@ -7,25 +7,21 @@ import json
 app = Flask(__name__)
 app.debug = True
 CORS(app)
+
 identifiant=['adrien']
 postSales=[]
 nombre = ['toto','tata','titi']
 
-@app.route("/", methods=["GET"])
-def getDefault():
-	global identifiant
-	print identifiant
-	return json.dumps(identifiant),200,{'Content-Type':'application/json'}
 
 @app.route("/retour", methods=["GET"])
-def getTest():
+def gettest():
 	global identifiant
 	print identifiant
 	return json.dumps(identifiant),200,{'Content-Type':'application/json'}
 
 
 @app.route("/rdm", methods=["GET"])
-def getRdm():
+def get():
 	global nombre
 	temp = random.choice(nombre)
 	return json.dumps(temp),200,{'Content-Type':'application/json'}
@@ -38,7 +34,7 @@ def getReset():
 
 
 @app.route("/sales",methods=["POST"])
-def postSales():
+def post():
 	global  postSales
 	postSales = request.get_json()
 	print postSales
@@ -47,15 +43,29 @@ def postSales():
 @app.route("/idPost",methods=["POST"])
 def postId():
 	global  identifiant
-	tmp = request.get_json()
-	#tmp = json.loads(tmp)
+	#tmp = request.get_json()
+	tmp = request.get_data()
+	tmp = json.loads(tmp)
 	identifiant.append(tmp)
 	print identifiant
 	return json.dumps(identifiant),200,{'Content-Type':'application/json'}
 
-@app.route("/idGet",methods=["GET"])
-def idGet():
-	return "test"
+@app.route("/idIsValide",methods=["POST"])
+def postIdIsValide():
+	global  identifiant
+	idvalide= request.get_data()
+	idvalide = json.loads(tmp)
+	for x in identifiant:
+		if idvalide in identifiant:
+			return json.dumps(idvalide),200,{'Content-Type':'application/json'}
+		else :continue
+	
+	print identifiant
+		
+
+#@app.route("/idGet",methods=["GET"])
+#def idGet():
+#	return "test"
 
 if __name__ == "__main__":
-	app.run()	
+app.run() 
