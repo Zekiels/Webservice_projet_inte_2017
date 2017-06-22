6331776ebb81cf64b94ad13127c3bc55861e15a8
horlaitheo <thorlait@yahoo.com>
2017-06-22 11:34:58 +0200

    mise en place du serveur phyton

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from pprint import pprint
+import random 
+import json
+
+app = Flask(__name__)
+app.debug = True
+CORS(app)
+identifiant=['adrien']
+postSales=[]
+nombre = ['toto','tata','titi']
+
+
+@app.route("/retour", methods=["GET"])
+def gettest():
+	global identifiant
+	print identifiant
+	return json.dumps(identifiant),200,{'Content-Type':'application/json'}
+
+
+@app.route("/rdm", methods=["GET"])
+def get():
+	global nombre
+	temp = random.choice(nombre)
+	return json.dumps(temp),200,{'Content-Type':'application/json'}
+
+@app.route("/reset", methods=["GET"])
+def getReset():
+	global nombre
+	temp = random.choice(nombre)
+	return json.dumps(temp),200,{'Content-Type':'application/json'}
+
+
+@app.route("/sales",methods=["POST"])
+def post():
+	global  postSales
+	postSales = request.get_json()
+	print postSales
+	return json.dumps(postSales),200,{'Content-Type':'application/json'}
+
+@app.route("/idPost",methods=["POST"])
+def postId():
+	global  identifiant
+	tmp = request.get_json()
+	#tmp = json.loads(tmp)
+	identifiant.append(tmp)
+	print identifiant
+	return json.dumps(identifiant),200,{'Content-Type':'application/json'}
+
+@app.route("/idGet",methods=["GET"])
+def idGet():
+	return "test"
+
+if __name__ == "__main__":
+	app.run()	
