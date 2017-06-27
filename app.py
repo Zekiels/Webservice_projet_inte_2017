6e5c452b26e2ffa9b35bb571415a91401c26fb49
horlaitheo <thorlait@yahoo.com>
2017-06-27 23:12:55 +0200

    create post actions

--- app.py
+++ app.py
@@ -267,7 +267,7 @@ def postAction(PlayerName):
 		day_tmp = day.pop()
 
 		db.execute("""
-	    INSERT INTO production VALUES ({0}, {1}, {2}, '{3}', {4});
+	    INSERT INTO production VALUES ({0}, {1}, {2}, '{3}', '{4}');
 	 	""".format(day_tmp.get("map_day_nb"), actions["actions"]["prepare"].values()[0], actions["actions"]["price"].values()[0], PlayerName, actions["actions"]["prepare"].items()[0][0]))
 		db.close()
 
