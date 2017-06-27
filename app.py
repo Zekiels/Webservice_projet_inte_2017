b573413324f1bcc4abccfd790375d4f0308f9cee
horlaitheo <thorlait@yahoo.com>
2017-06-27 14:01:50 +0200

    create get map

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def getMapPlayer():
 	listItems = []
 	realItemsByPlayer = {}
 	for i in player:
-
+		row = None
 		db.execute("""
 			SELECT mit_type, mit_pla_name, mit_longitude, mit_lattitude, mit_influence
 			FROM map_item
@@ -101,9 +101,12 @@ def getMapPlayer():
 		while row is not None:
 			items.update({"kind":row.get("mit_type"), "owner":row.get("mit_pla_name"), "location":{"lattitude":row.get("mit_lattitude"), "longitude":row.get("mit_longitude")},"influence":row.get("mit_influence")})
 			row = db.fetchone()
+					
 		listItems.append(items)
+
 		realItemsByPlayer.update({i.get("pla_name"):listItems})
 		print(realItemsByPlayer)
+	
 
 	#budget
 	playerInfo.append(db.select("""
