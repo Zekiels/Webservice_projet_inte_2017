d734b64414d30e7d9dda04b80d49d01298c35f31
awillaumez <aurelien.willaumez@imerir.com>
2017-06-28 01:45:34 +0200

    Signed-off-by: awillaumez <aurelien.willaumez@imerir.com>

--- app.py
+++ app.py
@@ -237,7 +237,7 @@ def postSales():
 	day_tmp = day.pop()
  	db.execute("""
  		INSERT INTO sale VALUES ({0}, {1}, '{2}', '{3}');
- 	""".format(day_tmp,sales['quantity'],sales['player'],sales['item']))
+ 	""".format(day_tmp['map_day_nb'],sales['quantity'],sales['player'],sales['item']))
         db.close()
 
  	return json.dumps("ok"),200,{'Content-Type':'application/json'}
