36bcb287e285ac7c98a8d671e5df12526048e816
Zekiels <mama.du33@live.fr>
2017-06-27 21:56:05 +0200

    Signed-off-by: Zekiels <mama.du33@live.fr>

--- app.py
+++ app.py
@@ -185,8 +185,8 @@ def postRejoindre():
     rejoindre = request.get_json()
     #Verifie si elle contient les infos necesaire
     print(rejoindre)
-    if "name" not in rejoindre :
-        return json_response({ "error" : "Missing name" }, 400)
+    #if "name" not in rejoindre :
+        #return json_response({ "error" : "Missing name" }, 400)
 	#Creation d'un nouveau joueur
 	#db = Db()
 	#budget = db.select("""SELECT pre_value FROM preference WHERE pre_name = 'budget';""")
