b6c3154f9f04d3907e77b5ed798eb231c904f664
bahanor <romain.drouard@imerir.com>
2017-06-28 11:13:52 +0200

    fixing route get map v2

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def getMap():
 		#itemsByPlayer)
 		oneItem_temp = db.select("SELECT mit_type AS kind, mit_pla_name AS owner, mit_longitude AS longitude, mit_lattitude AS lattitude, mit_influence AS influence FROM map_item WHERE mit_pla_name =\'" + i.get("name")+ "\';")
 		print(oneItem_temp)
-		if oneItem_temp.len() > 0 :
+		if len(oneItem_temp) > 0 :
 			oneItem = oneItem_temp[0]
 		else:
 			oneItem = oneItem_temp
