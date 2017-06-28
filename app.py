80f19963bbc1d6abb6001849a93fe17aa560378a
Zekiels <mama.du33@live.fr>
2017-06-28 17:30:08 +0200

    Signed-off-by: Zekiels <mama.du33@live.fr>

--- app.py
+++ app.py
@@ -351,7 +351,7 @@ def postWheather():
 	if weather["weather"][1]["dfn"] == 1:
 		previsionWeather = weather["weather"][1]["weather"]
 	db = Db()
-	day = ("SELECT map_day_nb FROM map;")[0]
+	day = ("SELECT map_day_nb FROM map;")
 	print (day)
 	#if (timestamp%24) == 0:
 	#	day = day + 1 
