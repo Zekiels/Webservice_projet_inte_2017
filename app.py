6cdf9152e55469396d78b3699fe4a1944274505d
bahanor <romain.drouard@imerir.com>
2017-06-28 10:48:21 +0200

    fixing route get map v2

--- app.py
+++ app.py
@@ -151,6 +151,7 @@ def getMapPlayer():
 		db = Db()
 		#itemsByPlayer)
 		oneItem = db.select("SELECT mit_type AS kind, mit_pla_name AS owner, mit_longitude AS longitude, mit_lattitude AS lattitude, mit_influence AS influence FROM map_item WHERE mit_pla_name =\'" + i.get("name")+ "\';")
+		print(oneItem)
 		listItems = {"kind":oneItem["kind"], "owner":oneItem["owner"], "location":{"lattitude":oneItem["lattitude"], "longitude":oneItem["longitude"]},"influence":oneItem["influence"]}
 		itemsByPlayer[i['name']] = listItems
 		db.close()
