830da76cc53631a7181791f2f536e6f5873b45ac
Zekiels <mama.du33@live.fr>
2017-06-28 13:36:35 +0200

    Signed-off-by: Zekiels <mama.du33@live.fr>

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ def postRejoindre():
 
 		db.close()
 
-	sqlCoord = (""" SELECT mit_longitude,mit_latitude FROM Map_Item WHERE mit_pla_name = @(nom) ;""",{"nom": name})
+	sqlCoord = (""" SELECT mit_longitude,mit_latitude FROM Map_Item WHERE mit_pla_name = '{0}' ;""".format(name))
 	coord = db.select(sqlCoord)
 	print(coord)
 	#sqlDrinksInfo = (""" SELECT * FROM recipe WHERE rcp_name = 'limonade';""")
