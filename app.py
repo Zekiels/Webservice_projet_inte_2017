43b3024c46c9deaf125a1ca43db8d108a2f62e3d
bahanor <romain.drouard@imerir.com>
2017-06-28 17:37:40 +0200

    fixing get map v3

--- app.py
+++ app.py
@@ -153,7 +153,19 @@ def getMap():
 		playerSales = playerSales_tmp[0]["sales"]
 
 		#profit
-		playerProfit_tmp = db.select("SELECT COALESCE((SELECT SUM (sal_qty * sal_price) FROM sale INNER JOIN player ON player.pla_name = sale.sal_pla_name WHERE sal_day_nb = {1} AND sal_pla_name = '{0}') - (SELECT SUM (pro_qty * pro_cost_at_that_time)),0) AS profit FROM production INNER JOIN player ON player.pla_name = production.pro_pla_name WHERE pro_day_nb = {1} AND pro_pla_name = '{0}' ) AS profit; ".format(i.get("name"), day.get("map_day_nb")))
+		playerProfit_tmp = db.select("""
+			SELECT COALESCE(
+				(SELECT SUM (sal_qty * sal_price) 
+				FROM sale INNER JOIN player ON player.pla_name = sale.sal_pla_name 
+				WHERE sal_day_nb = {1} AND sal_pla_name = '{0}') 
+				- 
+				(SELECT SUM (pro_qty * pro_cost_at_that_time)),0) AS profit 
+				FROM production 
+				INNER JOIN player ON player.pla_name = production.pro_pla_name 
+				WHERE pro_day_nb = {1} 
+				AND pro_pla_name = '{0}' ) AS profit; 
+			)
+		""".format(i.get("name"), day.get("map_day_nb")))
 		playerProfit = playerProfit_tmp[0]["profit"]
 
 		#drinksByPlayer
