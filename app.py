b02055a26621ed2b43c6a9e49589eaeeb7ca61f7
Zekiels <mama.du33@live.fr>
2017-06-29 11:13:11 +0200

    Test fonction..

--- app.py
+++ app.py
@@ -507,12 +507,12 @@ def postAction(PlayerName):
 #fonction qui permet de creer une pable vide pour chaque joueur a chaque fois q un nouveau jour commence 
 def createTab():
 	db = Db()
-	name = db.select("SELECT pla_name FROM player;")[0]
+	name = db.select("SELECT * FROM player;")
 	day = db.select("SELECT map_day_nb FROM map;")[0]["map_day_nb"] 
 	print(name)
 
-	for i in name :
-		print(name["pla_name"])
+	#for name in name["pla_name"]:
+		#print(name["pla_name"])
 
 if __name__ == "__main__":
  	app.run()
