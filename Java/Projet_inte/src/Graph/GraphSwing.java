dcc9e622cec007f6e01146708f74a75ac58fe3e3
adrien <adrien.corre@imerir.com>
2017-06-30 02:21:28 +0200

    add Java part in webservice git

---
+++ Java/Projet_inte/src/Graph/GraphSwing.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graph;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Théo
+ */
+public class GraphSwing extends Application {
+
+    
+    @Override
+    public void start(Stage primaryStage) {
+        /*primaryStage.setTitle("Hello World");
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 250, Color.LIGHTGREEN);
+        Button btn = new Button();
+        btn.setLayoutX(100);
+        btn.setLayoutY(80);
+        btn.setText("Hello World");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World");
+            }
+        });
+        root.getChildren().add(btn);        
+        primaryStage.setScene(scene);*/
+        primaryStage.setVisible(true);
+
+    }
+}
